2ee2869a80745ae3b1e1038ca142643cf650dc1b
Svet <mursvyats@gmail.com>
2023-11-27 04:00:36 +0500

    Playing with max and min widths and heights. Added color images.

--- car-design/src/components/Card.jsx
+++ car-design/src/components/Card.jsx
@@ -3,12 +3,12 @@ import OptionConstructor from "@/components/OptionConstructor";
 
 const Card = ({title, content, optionObject}) => {
     return (
-        <div className="card max-w-[30rem] max-h-56 m-[0_auto] bg-neutral text-neutral-content mt-[10rem]">
+        <div className="card max-w-[30rem] min-w-[22rem] min-h-56 m-[0_auto] bg-white shadow-2xl text-black mt-[10rem]">
             <div className="card-body items-center justify-center text-center">
                 <h2 className="card-title">{title}</h2>
                 <div className="flex gap-5 flex-wrap justify-center">
-                    {optionObject.map((_) => (
-                        <OptionConstructor/>
+                    {optionObject.map((element) => (
+                        <OptionConstructor imageSource={element.src}/>
                     ))}
                 </div>
                 <p>{content}</p>
--- car-design/src/components/OptionConstructor.jsx
+++ car-design/src/components/OptionConstructor.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Image from "next/image";
 
-const OptionConstructor = () => {
+const OptionConstructor = ({imageSource}) => {
     return (
         <div className="card-actions">
-            <button className="w-20 h-20 aspect-square rounded-full border-2 hover:border-amber-400"></button>
-            {/*<button className="btn btn-ghost">Deny</button>*/}
+            <button className="w-20 h-20 aspect-square rounded-full border-2 hover:border-amber-400">
+                <Image className="object-fill" src={imageSource} alt="image"/>
+            </button>
         </div>
     );
 };
