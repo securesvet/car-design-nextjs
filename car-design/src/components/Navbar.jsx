353f246794dd440aa01591a13e046a74fffc0190
Svet <mursvyats@gmail.com>
2023-11-27 20:34:20 +0500

    Added order button

--- car-design/src/components/Navbar.jsx
+++ car-design/src/components/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = ({logoImage, languageImage}) => {
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                         <li>
                             <details className="dropdown">
-                                <summary className="m-1">
+                                <summary className="h-16 w-16 flex justify-center items-center">
                                     <Image src={languageImage} alt="Language"
                                            className="block h-16 w-16 hover:shadow-md hover:cursor-pointer p-2 transition-shadow rounded-md"/>
                                 </summary>
-                                <ul className="relative right-1 p-2 shadow text-black menu dropdown-content z-[1] bg-white rounded-box w-40">
+                                <ul className="relative right-0 p-2 shadow text-black menu dropdown-content z-[1] bg-white rounded-box w-40">
                                     <li><a>English</a></li>
                                     <li><a>Russian</a></li>
                                 </ul>
